24bc32cbe168ca88cda10e2077ef08b9d3ec5578
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-15 02:31:50 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);   
+
+document.addEventListener("DOMContentLoaded", function() {
+  let p = document.querySelector('#text');
+  p.textContent = 'This is really cool!';
+});
